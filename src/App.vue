db345f39c2de76cbb46af368decba57f0184ea06
Augustin Bouverot <augustin.bouverot@gmail.com>
2025-01-15 03:17:45 +0100

    intial commit?

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Quizz from './components/Quizz.vue'
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+    <Quizz level=1 />
+    <Quizz level=1 />
+    <Quizz level=1 />
+    <Quizz level=1 />
+    <Quizz level=1 />
+    <Quizz level=1 />
+  </main>
+</template>
+
+<style scoped>
+</style>
